58badc673ed7816e8206b005ef411d58c0e33d59
Rafael Lemos <rafael_plemos@hotmail.com>
2023-08-22 16:57:42 -0300

    TestCommit

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+start_height = 100 
+num_bounce = 0
+
+while num_bounce <= 10:
+    print(round(start_height, ndigits=4))
+    start_height = start_height * 0.6
+    num_bounce += 1
